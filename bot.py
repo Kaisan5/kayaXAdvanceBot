a580e4e3cf52473e9948d304d4e641b1c8e5cb78
Kai <131925584+Kaisan5@users.noreply.github.com>
2025-05-04 02:56:38 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -84,7 +84,7 @@ class Bot(Client):
                                           """)
         self.set_parse_mode(ParseMode.HTML)
         self.username = usr_bot_me.username
-        self.LOGGER(__name__).info(f"Bot Running..! Made by @Codeflix_Bots")   
+        self.LOGGER(__name__).info(f"Bot Running..!")   
 
         # Start Web Server
         app = web.AppRunner(await web_server())
@@ -92,7 +92,7 @@ class Bot(Client):
         await web.TCPSite(app, "0.0.0.0", PORT).start()
 
 
-        try: await self.send_message(OWNER_ID, text = f"<b><blockquote> Bᴏᴛ Rᴇsᴛᴀʀᴛᴇᴅ by @Codeflix_Bots</blockquote></b>")
+        try: await self.send_message(OWNER_ID, text = f"<b><blockquote> Bᴏᴛ Rᴇsᴛᴀʀᴛᴇᴅ by @EternalsHelplineBot</blockquote></b>")
         except: pass
 
     async def stop(self, *args):

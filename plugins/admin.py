d667fad115bdb68f0c8359c21a8d0ebb3b5fc688
Kai <131925584+Kaisan5@users.noreply.github.com>
2025-05-03 18:03:11 +0530

    Update admin.py

--- plugins/admin.py
+++ plugins/admin.py
@@ -123,3 +123,4 @@ async def get_admins(client: Client, message: Message):
 
     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("ᴄʟᴏsᴇ", callback_data="close")]])
     await pro.edit(f"<b>⚡ Current Admin List:</b>\n\n{admin_list}", reply_markup=reply_markup)
+    #Stelleron_Hunter

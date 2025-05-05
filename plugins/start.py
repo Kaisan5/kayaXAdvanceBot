4457dc7590aa204baac742239a1163ba74c94f49
Kai <131925584+Kaisan5@users.noreply.github.com>
2025-05-05 22:10:42 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -214,11 +214,9 @@ async def not_joined(client: Client, message: Message):
                                 expire_date=datetime.utcnow() + timedelta(seconds=FSUB_LINK_EXPIRY) if FSUB_LINK_EXPIRY else None)
                             link = invite.invite_link
 
-                    buttons.append([
-                    InlineKeyboardButton("•Jᴏɪɴ ᴄʜᴀɴɴᴇʟ•", url=link)
-                 ])
+                    buttons.append([InlineKeyboardButton("•Jᴏɪɴ ᴄʜᴀɴɴᴇʟ•", url=link)])
                     count += 1
-                    await temp.edit(f"<b>{'! ' * count}</b>")
+                    await temp.edit(f"<b>{'? ' * count}</b>")
 
                 except Exception as e:
                     print(f"Error with chat {chat_id}: {e}")
